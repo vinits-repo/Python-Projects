ab14a0eadf935440c625e202138d6d030c4eff56
Vinit Yadav <143257920+vinits-repo@users.noreply.github.com>
2023-10-26 10:59:03 +0530

    Add files via upload

---
+++ QR Code Generator/QRCode.py
@@ -0,0 +1,3 @@
+import qrcode as qr
+img = qr.make("https://youtu.be/dku9ECJftDw?si=2uUFkHNiwlx6uEAu")
+img.save("HariStotram.png")
---
+++ QR Code Generator/QRCode2.py
@@ -0,0 +1,10 @@
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(version=1,
+                   error_correction=qrcode.constants.ERROR_CORRECT_H,
+                   box_size=10,border=4,)
+qr.add_data("https://www.python.org/")
+qr.make(fit=True)
+img = qr.make_image(back_color="lightblue")
+img.show()
