1597e571e741a9186a7ded00cfd714c3d3ddc756
Vinit Yadav <143257920+vinits-repo@users.noreply.github.com>
2023-10-30 08:38:16 +0530

    Add files via upload

---
+++ Rock, Paper and Scissor/main.py
@@ -0,0 +1,21 @@
+import random
+
+def play():
+    user = input("Enter your choice, 'r' for rock, 'p' for paper, 's' for scissor: ")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'It\'s a tie'
+    
+    if is_win(user, computer):
+        return 'You won!'
+    
+    return 'You lost!'
+
+def is_win(player, opponent):
+    # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
+        return True
+    
+    
+print(play())
